ddf549573043f61b4d550a93cf2dd106a9d281f2
T Venkata Maheswara Reddy <tvenkatamaheshreddy@gmail.com>
2020-12-03 10:48:53 +0530

    Update comment-feed.component.ts

--- src/app/comment-feed/comment-feed.component.ts
+++ src/app/comment-feed/comment-feed.component.ts
@@ -44,6 +44,29 @@ export class CommentFeedComponent implements OnInit {
       // .subscribe(console.log);
   }
 
+  
+//   const getData = (param) => {
+//   return of(`retrieved new data with param ${param}`).pipe(
+//     delay(1000)
+//   )
+// }
+
+// // using a regular map
+// from([1,2,3,4]).pipe(
+//   map(param => getData(param))
+// ).subscribe(val => val.subscribe(data => console.log(data)));
+
+// // using map and mergeAll
+// from([1,2,3,4]).pipe(
+//   map(param => getData(param)),
+//   mergeAll()
+// ).subscribe(val => console.log(val));
+
+// // using mergeMap
+// from([1,2,3,4]).pipe(
+//   mergeMap(param => getData(param))
+// ).subscribe(val => console.log(val));
+
   /*
   * Intiale form
   */
